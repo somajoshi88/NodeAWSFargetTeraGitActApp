ca4b7be687771883e54b0df8ec3655efa43dc8ac
somajoshi <somacse.joshi@gmail.com>
2024-06-09 16:39:45 +0530

    remove aws secret

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAYS2NSILMVIWJLKVP"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
